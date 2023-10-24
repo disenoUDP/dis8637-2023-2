613421f702374e320df2766939588e83071947aa
aarón montoya-moraga <montoyamoraga@gmail.com>
2023-10-23 21:53:59 -0300

    agregar ejemplo

---
+++ clases/clase-10/recetas/sketch_oct23a/creditos.ino
@@ -0,0 +1,3 @@
+// recetas hechas para el curso dis8637
+// dictado por andres martin, sergio majluf, aaron montoya
+// en diseno UDP, segundo semestre 2023
---
+++ clases/clase-10/recetas/sketch_oct23a/sketch_oct23a.ino
@@ -0,0 +1,18 @@
+
+// variables globales
+bool modoPrueba = false;
+
+// setup() ocurre al principio, una vez
+void setup() {
+
+  // inicializar comunicacion serial
+  Serial.begin(9600);
+
+
+}
+
+// loop() ocurre después de setup(), en bucle
+void loop() {
+  
+
+}
