2de69cd040a24ce452522f475e32a23bb909dd66
Andrés Martin Mena <128084150+AndresMartinM@users.noreply.github.com>
2023-10-24 15:41:19 -0300

    Add files via upload

--- clases/clase-10/recetas/curva-231023/curva-231023.ino
+++ clases/clase-10/recetas/curva-231023/curva-231023.ino
@@ -5,10 +5,11 @@
 // codigo para hacer cambios en valores segun el tiempo
 // sirve para hacer fade-in fade-out y tintineo
 //
+// abran el serial plotter para visualizar valorAnim
+//
 // por AndresMartinM para el curso Taller UX de UDP
 // primavera 2023
 
-// IMPORTANTE AUN NO ESTA FUNCIONANDO
 
 // pin del boton para iniciar la animacion
 int btn = 8;
@@ -16,19 +17,27 @@ int btn = 8;
 // variable que se usara como punto de partida de la animacion
 long inicio = -1;
 // duracion en milisegundos
-int duracion = 5000;
+int duracion = 1000;
+bool direccion = false;
 
-int valorAnim = 0; // este valor vamos a animar
+int valorAnim = 0;   // este valor vamos a animar
+
+bool ocurriendo = false;
 
 void setup() {
   Serial.begin(9600);
   pinMode(btn, INPUT);
+  pinMode(3, OUTPUT);
 }
 
 void loop() {
-  
+  //Serial.print(millis()-inicio);
   Serial.println(valorAnim);
+
   evento();
+
+  analogWrite(3, valorAnim);
+
 }
 
 // necesito una funcion que me entregue valores entre x e y, y que los 'reproduzca' en un tiempo especifico
@@ -38,44 +47,81 @@ void loop() {
 // cortar la animacion
 // esperar evento
 
-bool ocurriendo = false;
-
-bool boton() {
-  if (digitalRead(btn) == HIGH) {
+// funcion booleana que nos dice si el boton esta presionado o no (no contiene debounce)
+bool boton(int b) {
+  if (digitalRead(b) == HIGH) {
     return true;
-    Serial.println("vamos biem 0");
   } else {
     return false;
-    Serial.println("vamos biem -1");
   }
 }
 
+// dentro de la funcion evento se decide que etapa del 
 void evento() {
-  if (boton() && !ocurriendo) {
+  if (boton(btn) && !ocurriendo) {
     inicio = millis();
-    Serial.println("vamos biem");
     ocurriendo = true;
-  } else if((millis() - inicio) < duracion){
-    valorAnim = anim(duracion);
-    Serial.println("vamos biem2");
+    if (direccion) {
+      direccion = false;
+    } else {
+      direccion = true;
+    }
+  } else if ((millis() - inicio) < duracion && ocurriendo) {
+    valorAnim = animQuad(duracion, direccion); // aca ponemos cual animacion usaremos
   } else {
     ocurriendo = false;
-    Serial.println("vamos biem3");
+  }
+  if(!ocurriendo){
+    if (direccion){
+      valorAnim = 255;
+    } else {
+      valorAnim = 0;
+    }
   }
 }
 
-int anim(int dur) {
+
+int animLine(int dur, bool dir) {
   int time = millis() - inicio;
-  int max = 128;
-  int funcion = cubeIn(map(time, 0, dur, 0, max)); // aca ponemos la funcion matematica que vamos a usar, como duracion es un valor muy alto lo mapee dentro de la funcion
-  return map(funcion, 0, cubeIn(max), 0, 512); // aca puse entre 0 y 512 pero pueden poner el rango que necesiten
+  int max = 255;
+  int funcion;
+  if (dir) {
+    funcion = lineIn(map(time, 1, dur, 0, max));
+  } else {
+    funcion = lineIn(map(time, 1, dur, max, 0));
+  }
+  return funcion;
+}
+int animQuad(int dur, bool dir) {
+  int time = millis() - inicio;
+  int max = 32;
+  int funcion = quadIn(map(time, 0, dur, 0, max));  // aca ponemos la funcion matematica que vamos a usar, como duracion es un valor muy alto lo mapee dentro de la funcion
+  if (dir) {
+    return map(funcion, quadIn(1), quadIn(max), 0, 255);  // aca puse entre 0 y 255 pero pueden poner el rango que necesiten
+  } else {
+    return map(funcion, quadIn(1), quadIn(max), 255, 0);  // aca puse entre 0 y 255 pero pueden poner el rango que necesiten
+  }
+}
+// por la pendiente tan alta de la curva el arduino casi no se la puede (cuidado)
+int animCube(int dur, bool dir) {
+  int time = millis() - inicio;
+  int max = 12;
+  int funcion = cubeIn(map(time, 0, dur, 0, max));  // aca ponemos la funcion matematica que vamos a usar, como duracion es un valor muy alto lo mapee dentro de la funcion
+  if (dir) {
+    return map(funcion, cubeIn(1), cubeIn(max), 0, 255);  // aca puse entre 0 y 255 pero pueden poner el rango que necesiten
+  } else {
+    return map(funcion, cubeIn(1), cubeIn(max), 255, 0);  // aca puse entre 0 y 255 pero pueden poner el rango que necesiten
+  }
 }
 
 // a continuacion las funciones matematicas usadas
+int lineIn(int t) {
+  return t;
+}
+int quadIn(int t) {
+  return (t * t);
+}
 int cubeIn(int t) {
   return (t * t * t);
 }
 
-int cubeOut(int t) {
-  return 1 + (--t) * t * t;
-}
