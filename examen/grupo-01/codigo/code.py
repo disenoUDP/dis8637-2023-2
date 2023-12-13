2e7c6ba357e412bc76526321a1afcebc7b56b491
aarón montoya-moraga <montoyamoraga@gmail.com>
2023-12-12 22:05:40 -0300

    agregar codigo python

--- examen/grupo-01/codigo/code.py
+++ examen/grupo-01/codigo/code.py
@@ -1,28 +1,3 @@
-# examen grupo-01
-
-## integrantes
-
-* cooper manzor, valentina
-* frías gonzález, aranza ignacia
-* huerta galdames, sofía paz
-
-## descripción
-
-Un instrumento portable en una cangurera, la idea es inducir a la relajación, a tener una experiencia emocional y sensorial en el usuario.
-
-## imágenes
-
-![1](./imagenes/1.jpg)
-
-![2](./imagenes/2.jpg)
-
-![3](./imagenes/3.jpg)
-
-![4](./imagenes/4.jpg)
-
-## código
-
-```python
 # SPDX-FileCopyrightText: 2019 Kattni Rembor for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
@@ -75,4 +50,3 @@ while True:
             play_file(audiofiles[i])
 
     time.sleep(0.01)
-```
